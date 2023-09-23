2920928e4883c30b05e6c8800edfc338c45b5e27
KramDarya <dkramar936@gmail.com>
2023-09-23 15:58:21 +0600

    Книги

--- app.py
+++ app.py
@@ -104,6 +104,18 @@ def example ():
         {'name': 'мандарины', 'price': 95},
         {'name': 'манго', 'price': 321}
     ]
+    books = [
+        {'avtor': 'Виктория Авеярд', 'name': 'Разрушенный трон', 'genre': 'Фэнтези', 'str': 220},
+        {'avtor': 'Рагнар Йонассон', 'name': 'Затмение', 'genre': 'Детектив', 'str': 240},
+        {'avtor': 'Роберт Сперанский', 'name': 'Главный критерий', 'genre': 'Психология', 'str': 330},
+        {'avtor': 'Хелена Побяржина', 'name': 'Валсарб', 'genre': 'Проза', 'str': 150},
+        {'avtor': 'Рагим Джафаров', 'name': 'Саго', 'genre': 'Триллер', 'str': 410},
+        {'avtor': 'Дмитрий Строгов', 'name': 'Тысячелетие обмана', 'genre': 'История', 'str': 287},
+        {'avtor': 'Николай Бель', 'name': 'росчерк на колене', 'genre': 'Научная литература', 'str': 356},
+        {'avtor': 'Артем Вожаков', 'name': 'Интеллектуальные информационные системы', 'genre': 'Бизнес-литература', 'str': 240},
+        {'avtor': 'Александр Низаев', 'name': 'Космический наемник', 'genre': 'Фантастика', 'str': 260},
+        {'avtor': 'Кейт Хаск', 'name': 'Дикость', 'genre': 'Роман', 'str': 210},
+    ]
     return render_template('example.html',
                             name=name, nomerl=nomerl, gruppa=gruppa,
-                            nomerk=nomerk, fruits=fruits)
+                            nomerk=nomerk, fruits=fruits, books=books)
