37231b787eeee4f45f34f00065d3d917493cd855
KramDarya <dkramar936@gmail.com>
2023-10-21 00:32:47 +0600

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -1,40 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
-@app.route("/lab2/example")
-def example ():
-    name, nomerl, gruppa, nomerk = 'Крамар Дарья, Проворова Елена', '2', '11', '3'
-    fruits = [
-        {'name': 'яблоки', 'price': 100},
-        {'name': 'груши', 'price': 120},
-        {'name': 'апельсины', 'price': 80},
-        {'name': 'мандарины', 'price': 95},
-        {'name': 'манго', 'price': 321}
-    ]
-    books = [
-        {'avtor': 'Виктория Авеярд', 'name': 'Разрушенный трон', 'genre': 'Фэнтези', 'str': 220},
-        {'avtor': 'Рагнар Йонассон', 'name': 'Затмение', 'genre': 'Детектив', 'str': 240},
-        {'avtor': 'Роберт Сперанский', 'name': 'Главный критерий', 'genre': 'Психология', 'str': 330},
-        {'avtor': 'Хелена Побяржина', 'name': 'Валсарб', 'genre': 'Проза', 'str': 150},
-        {'avtor': 'Рагим Джафаров', 'name': 'Саго', 'genre': 'Триллер', 'str': 410},
-        {'avtor': 'Дмитрий Строгов', 'name': 'Тысячелетие обмана', 'genre': 'История', 'str': 287},
-        {'avtor': 'Николай Бель', 'name': 'росчерк на колене', 'genre': 'Научная литература', 'str': 356},
-        {'avtor': 'Артем Вожаков', 'name': 'Интеллектуальные информационные системы', 'genre': 'Бизнес-литература', 'str': 240},
-        {'avtor': 'Александр Низаев', 'name': 'Космический наемник', 'genre': 'Фантастика', 'str': 260},
-        {'avtor': 'Кейт Хаск', 'name': 'Дикость', 'genre': 'Роман', 'str': 210},
-    ]
-    return render_template('example.html',
-                            name=name, nomerl=nomerl, gruppa=gruppa,
-                            nomerk=nomerk, fruits=fruits, books=books)
-@app.route("/lab2/")
-def lab2 ():
-    return render_template('lab2.html')
-@app.route("/lab2/series")
-def series ():
-    return render_template('series.html')
+
+
     
--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template
 lab1 = Blueprint('lab1', __name__)
 
 
@@ -26,7 +26,9 @@ def menu ():
             <li><a href="/lab1" target="_blank">
                 Лабораторная работа 1</a></li> 
             <li><a href="/lab2/" target="_blank">
-                Лабораторная работа 2</a></li>   
+                Лабораторная работа 2</a></li> 
+            <li><a href="/lab3/" target="_blank">
+                Лабораторная работа 3</a></li> 
         </ol>
     </main>
         <h1>web-сервер на flask</h1>
@@ -103,40 +105,3 @@ def nstu ():
     </body>
 </html>
 '''
-
-
-@lab1.route("/lab2/example")
-def example ():
-    name, nomerl, gruppa, nomerk = 'Крамар Дарья, Проворова Елена', '2', '11', '3'
-    fruits = [
-        {'name': 'яблоки', 'price': 100},
-        {'name': 'груши', 'price': 120},
-        {'name': 'апельсины', 'price': 80},
-        {'name': 'мандарины', 'price': 95},
-        {'name': 'манго', 'price': 321}
-    ]
-    books = [
-        {'avtor': 'Виктория Авеярд', 'name': 'Разрушенный трон', 'genre': 'Фэнтези', 'str': 220},
-        {'avtor': 'Рагнар Йонассон', 'name': 'Затмение', 'genre': 'Детектив', 'str': 240},
-        {'avtor': 'Роберт Сперанский', 'name': 'Главный критерий', 'genre': 'Психология', 'str': 330},
-        {'avtor': 'Хелена Побяржина', 'name': 'Валсарб', 'genre': 'Проза', 'str': 150},
-        {'avtor': 'Рагим Джафаров', 'name': 'Саго', 'genre': 'Триллер', 'str': 410},
-        {'avtor': 'Дмитрий Строгов', 'name': 'Тысячелетие обмана', 'genre': 'История', 'str': 287},
-        {'avtor': 'Николай Бель', 'name': 'росчерк на колене', 'genre': 'Научная литература', 'str': 356},
-        {'avtor': 'Артем Вожаков', 'name': 'Интеллектуальные информационные системы', 'genre': 'Бизнес-литература', 'str': 240},
-        {'avtor': 'Александр Низаев', 'name': 'Космический наемник', 'genre': 'Фантастика', 'str': 260},
-        {'avtor': 'Кейт Хаск', 'name': 'Дикость', 'genre': 'Роман', 'str': 210},
-    ]
-    return render_template('example.html',
-                            name=name, nomerl=nomerl, gruppa=gruppa,
-                            nomerk=nomerk, fruits=fruits, books=books)
-
-
-@lab1.route("/lab2/")
-def lab2 ():
-    return render_template('lab2.html')
-
-
-@lab1.route("/lab2/series")
-def series ():
-    return render_template('series.html')
